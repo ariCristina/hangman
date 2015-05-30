0403845dd1088baf05b721edea8af3dc9d4ffd0b
sergiucaraian <sergiu.caraian@gmail.com>
2015-05-30 17:17:08 +0300

    Added Lobby search

--- app/src/main/java/donthang/hangman/LobbyActivity.java
+++ app/src/main/java/donthang/hangman/LobbyActivity.java
@@ -1,18 +1,56 @@
 package donthang.hangman;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class LobbyActivity extends Activity {
+    static JSONObject credentials;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+        requestQueue = Volley.newRequestQueue(this);
+
+        String creds = getIntent().getStringExtra("credentials");
+
+        try {
+            credentials = new JSONObject(creds);
+        } catch (JSONException e) {
+            Toast.makeText(getApplicationContext(),"Json error",Toast.LENGTH_LONG).show();
+        }
+
+        POSTFunctions
+        final Button searchButton = (Button)findViewById(R.id.btnSearch);
+        final EditText searchEditText = (EditText) findViewById(R.id.search);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = searchEditText.getText().toString();
+                try {
+                    POSTFunctions.searchUser(LobbyActivity.this, requestQueue,
+                            credentials.getString("user_id"), credentials.getString("access_token"),
+                            searchEditText.getText().toString());
+                } catch (JSONException e) {};
+                Toast.makeText(getApplicationContext(), "Unable to find user", Toast.LENGTH_LONG);
+            }
+        });
     }
 
     @Override
@@ -36,4 +74,6 @@ public class LobbyActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
+
--- app/src/main/java/donthang/hangman/POSTFunctions.java
+++ app/src/main/java/donthang/hangman/POSTFunctions.java
@@ -12,6 +12,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -38,11 +39,60 @@ public class POSTFunctions {
                             if (jsonMeta.getString("status").equals("200")){
                                 Intent intent = new Intent(activity,LobbyActivity.class);
                                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.putExtra("credentials",response.toString());
                                 activity.startActivity(intent);
                                 activity.finish();
                             }
                             else {
                                 Toast.makeText(activity, "Unregistered user", Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(activity, "ERROR", Toast.LENGTH_LONG).show();
+                    }
+                }) {
+
+            @Override
+            public Map<String,String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+        };
+        jsonObjReq.setTag(tag_login);
+        requestQueue.add(jsonObjReq);
+    }
+
+    public static void logout(final Activity activity, RequestQueue requestQueue, String user_id, String access_token) {
+        String tag_login = "logout";
+        Map<String,String> jsonParams = new HashMap<String,String>();
+
+        jsonParams.put("user_id",user_id);
+        jsonParams.put("access_token", access_token);
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.POST,
+                activity.getResources().getString(R.string.rest_url_logout),
+                new JSONObject(jsonParams),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject jsonMeta = new JSONObject(response.getString("meta"));
+                            if (jsonMeta.getString("status").equals("200")){
+                                Intent intent = new Intent(activity,MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                activity.startActivity(intent);
+                                activity.finish();
+                            }
+                            else {
+                                Toast.makeText(activity, "Can't log out", Toast.LENGTH_LONG).show();
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -85,8 +135,9 @@ public class POSTFunctions {
                         try {
                             JSONObject jsonMeta = new JSONObject(response.getString("meta"));
                             if (jsonMeta.getString("status").equals("201")){
-                                Intent intent = new Intent(activity,LobbyActivity.class);
+                                Intent intent = new Intent(activity,MainActivity.class);
                                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                Toast.makeText(activity,"Registered succesfully",Toast.LENGTH_LONG);
                                 activity.startActivity(intent);
                                 activity.finish();
                             }
@@ -115,5 +166,74 @@ public class POSTFunctions {
         jsonObjReq.setTag(tag_register);
         requestQueue.add(jsonObjReq);
     }
+
+    public static void searchUser(final Activity activity, RequestQueue requestQueue,
+                                    final String user_id, final String access_token,
+                                    final String keyphrase) {
+        String tag_lobby = "lobby";
+        Map<String,String> jsonParams = new HashMap<String,String>();
+
+        jsonParams.put("user_id",user_id);
+        jsonParams.put("access_token", access_token);
+        jsonParams.put("filter", "3");
+        jsonParams.put("keyphrase", keyphrase);
+        jsonParams.put("status", "online");
+
+        final Boolean foundUser = false;
+        final String foundUserID = null;
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.POST,
+                activity.getResources().getString(R.string.rest_url_get_lobby),
+                new JSONObject(jsonParams),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Toast.makeText(activity,"DBG",Toast.LENGTH_LONG).show();
+                        try {
+                            JSONObject jsonMeta = new JSONObject(response.getString("meta"));
+                            if (jsonMeta.getString("status").equals("200")){
+                                JSONArray users = response.getJSONArray("content");
+                                for(int i = 0; i<users.length(); i++) {
+                                    JSONObject usr = users.getJSONObject(i);
+
+                                    if(usr.getString("nickname").equals(keyphrase)) {
+                                        Intent intent = new Intent(activity,CreateChallengeActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.putExtra("user_id_challenger", user_id);
+                                        intent.putExtra("access_token_challenger", access_token);
+                                        intent.putExtra("userid_challenged", usr.getString("user_id"));
+                                        activity.startActivity(intent);
+                                    }
+                                }
+                                Toast.makeText(activity, "Could not find user", Toast.LENGTH_LONG).show();
+                            }
+                            else {
+                                Toast.makeText(activity, "Error getting users", Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(activity, "ERROR", Toast.LENGTH_LONG).show();
+                    }
+                }) {
+
+            @Override
+            public Map<String,String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+        };
+        jsonObjReq.setTag(tag_lobby);
+        requestQueue.add(jsonObjReq);
+    }
+c
 }
 
